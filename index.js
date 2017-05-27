831d4245bf4257b6f75736ecb5c3dacc43629da7
bljconez <bconez@up.edu.ph>
2017-05-27 22:59:21 +0800

    2nd commit

---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const database = require('./database');
+const File = require('./models').File;
+const User = require('./models').User;
+const flash = require('express-flash');
+var bodyparser = require('body-parser');
+const consolidate = require('consolidate');
+const cookieparser = require('cookie-parser');
+const session = require('express-session');
+const middlewares = require("middlewares");
+const multer = require('multer');
+const path = require('path');
+var app = express();
+
+
+app.engine('html', consolidate.nunjucks);
+app.set('views', './views');
+
+// app.use(bodyparser.urlencoded());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cookieparser('secret-cookie'));
+app.use(session({ resave: false, saveUninitialized: false, secret: 'secret-cookie' }));
+app.use(flash());
+
+app.use('/static', express.static('./static'));
+app.use('/avatars', express.static('./avatar_pics'));
+app.use(require('./auth'));
+
+
+var user = function retrieveSignedInUser(req, res, next) {
+    req.user = req.session.currentUser;
+    next();
+}
+
+
+app.use(user);
+
+
+
+
+app.get('/profile', requireSignedIn, function(req, res) {
+	const email = req.user;
+	const name = email;
+	User.findOne({ where: {name: name} }).then(function(user) {
+		res.render('profile.html', {
+			user: user
+		});
+	});
+});
+
+app.get('/profile',function(req, res) {
+	// const email = req.user;
+	// const name = email;
+	// User.findOne({ where: {name: name} }).then(function(user) {
+	// 	res.render('profile.html', {
+	// 		user: user
+	// 	});
+	// });
+
+	res.render('profile.html');
+});
+
+app.get('/', function(req, res) {
+	console.log("HELLLOO");
+	res.render('index.html');
+});
+
+app.get('/course', function(req, res) {
+	// const email = req.user;
+	// const name = email;
+	// User.findOne({ where: {name: name} }).then(function(user) {
+	// 	res.render('profile.html', {
+	// 		user: user
+	// 	});
+	// });
+	res.render('course.html');
+});
+
+app.get('/files',  function(req, res) {
+	// const email = req.user;
+	// const name = email;
+	// User.findOne({ where: {name: name} }).then(function(user) {
+	// 	res.render('profile.html', {
+	// 		user: user
+	// 	});
+	// });
+	res.render('files.html');
+});
+
+const upload = multer({dest: './uploads'})
+
+app.post('/upload-avatar', requireSignedIn, upload.single('avatar'), function(req, res){
+
+	const email = req.user;
+
+	User.findOne({ where: { email: email } }).then(function(user) {
+		user.update({avatar: '/avatars/' + req.file.filename}).then(function(){
+			res.redirect('/profile');
+			console.log("HAHAA " + user.avatar);
+		});
+		 
+	});
+
+	
+});
+
+
+
+function requireSignedIn(req, res, next) {
+    if (!req.session.currentUser) {
+        return res.redirect('/');
+    }
+    next();
+}
+
+
+
+app.listen(3000, function() {
+	console.log('Server is now running at port 3000');
+});
---
+++ models.js
@@ -0,0 +1,67 @@
+const Sequelize = require('sequelize');
+const database = require('./database');
+
+const User= database.define('users', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    name:{
+        type: Sequelize.STRING,
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    avatar: {
+        type: Sequelize.STRING,
+
+    }
+}, {
+    timestamps: true
+});
+
+
+
+const File = database.define('files', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    name:{
+        type: Sequelize.STRING,
+    },
+    course:{
+        type: Sequelize.STRING,
+    },
+    user_id: {
+        type: Sequelize.INTEGER,
+        foreignKey: true,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    }
+}, {
+    timestamps: true
+});
+
+// User.hasMany(Account, { foreignKey: 'user_id' });
+File.belongsTo(User, { foreignKey: 'user_id' });
+// Account.belongsTo(User, { foreignKey: 'user_id' , foreignKeyConstraint:true });
+
+database.sync();
+
+ module.exports.File = File;
+
+module.exports.User = User;
+// module.exports.Account = Account;
